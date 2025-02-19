8f850ac2e011b311fe9ac8f07a967210cf70b9e7
Al Boucetta <alboucetta@Als-MacBook-Pro.local>
2025-02-19 11:41:06 -0500

    green header + font change

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,16 @@ async function Home() {
   return (
     <div>
     <header className="bg-[#004C54] text-white py-8 mb-8">
-      
+    <div className="container mx-auto px-4">
+          <h1 className="text-4xl font-bold text-center">
+            Super Special Awesome Blog Posts
+          </h1>
+          <div className="h-1 w-32 bg-[#A5ACAF] mx-auto mt-4"></div>
+        </div>
     </header>
 
     <main className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">Blog Posts</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center">Recent Posts</h1>
       
       <div className="max-w-2xl mx-auto">
         <ul className="space-y-4">
--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -3,10 +3,20 @@ async function PostPage({ params }) {
     const post = await response.json();
 
     return (
+        <div>
+        <header className="bg-[#004C54] text-white py-8 mb-8">
+        <div className="container mx-auto px-4">
+            <h1 className="text-4xl font-bold text-center">
+                Super Special Awesome Blog Posts
+            </h1>
+            <div className="h-1 w-32 bg-[#A5ACAF] mx-auto mt-4"></div>
+            </div>
+        </header>
         <main className="container mx-auto px-4 py-8">
             <h1>{post.title}</h1>
             <p>{post.body}</p>
         </main>
+        </div>
     );
 }
 
