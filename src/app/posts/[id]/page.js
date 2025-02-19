ef6c12620cad7650241101a3a4200d8a036c4849
Al Boucetta <alboucetta@Als-MacBook-Pro.local>
2025-02-19 11:13:41 -0500

    post title and post content fetched

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -4,7 +4,8 @@ async function PostPage({ params }) {
 
     return (
         <main className="container mx-auto px-4 py-8">
-            <h1>hi there! pls be working</h1>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
         </main>
     );
 }
